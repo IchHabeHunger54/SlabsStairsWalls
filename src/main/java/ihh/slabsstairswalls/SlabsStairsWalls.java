a7031615aa25e3848cda93e91039e5d1d347b18a
IchHabeHunger54 <kingdavidaut@gmail.com>
2020-04-12 19:01:14 +0200

    release

--- src/main/java/ihh/slabsstairswalls/SlabsStairsWalls.java
+++ src/main/java/ihh/slabsstairswalls/SlabsStairsWalls.java
@@ -31,6 +31,53 @@ public class SlabsStairsWalls {
     }
 
     private void clientSetup(final FMLClientSetupEvent event) {
-//        RenderTypeLookup.setRenderLayer(MinecraftBlocks.GLASS_SLAB.get(), RenderType.getCutoutMipped());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.WHITE_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.WHITE_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.WHITE_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.ORANGE_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.ORANGE_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.ORANGE_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.MAGENTA_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.MAGENTA_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.MAGENTA_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.LIGHT_BLUE_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.LIGHT_BLUE_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.LIGHT_BLUE_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.YELLOW_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.YELLOW_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.YELLOW_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.LIME_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.LIME_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.LIME_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.GRAY_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.GRAY_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.GRAY_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.LIGHT_GRAY_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.LIGHT_GRAY_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.LIGHT_GRAY_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.CYAN_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.CYAN_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.CYAN_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.PURPLE_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.PURPLE_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.PURPLE_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.BLUE_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.BLUE_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.BLUE_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.BROWN_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.BROWN_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.BROWN_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.GREEN_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.GREEN_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.GREEN_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.RED_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.RED_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.RED_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.BLACK_STAINED_GLASS_SLAB.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.BLACK_STAINED_GLASS_STAIRS.get(), RenderType.getTranslucent());
+        RenderTypeLookup.setRenderLayer(MinecraftBlocks.BLACK_STAINED_GLASS_WALL.get(), RenderType.getTranslucent());
     }
 }
